e96e8e9ea91a5321a5b5fce12e8232344806fa4f
wkxxyy <5794431@qq.com>
2019-05-28 17:53:25 +0800

    2

--- Dao.go
+++ Dao.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+	"log"
+)
 
 func main() {
-
+	db, err := sql.Open("mysql", "user:password@tcp(ip:port)/database")
+	if err != nil {
+		log.Println(err)
+	}
+	//在这里进行一些数据库操作
+	defer db.Close()
 
 }
